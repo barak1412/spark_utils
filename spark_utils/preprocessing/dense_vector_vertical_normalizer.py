fe7c03ae7ccb1e44734bc100df4abb2710892b51
barak1412 <barak1413@gmail.com>
2023-06-18 19:36:15 +0300

    Fixed support for mllib vectors.

--- spark_utils/preprocessing/dense_vector_vertical_normalizer.py
+++ spark_utils/preprocessing/dense_vector_vertical_normalizer.py
@@ -4,6 +4,7 @@ from pyspark.ml.linalg import DenseVector, VectorUDT
 
 
 _abs_dense_vector_udf = F.udf(lambda v: DenseVector([abs(val) for val in v]), VectorUDT())
+_convert_mllib_to_ml = F.udf(lambda v: v.asML(), VectorUDT())
 
 
 class DenseVectorVerticalNormalizer(object):
@@ -18,12 +19,17 @@ class DenseVectorVerticalNormalizer(object):
         if first_row is None:
             return df.selectExpr('*', f'{self._inputCol} as {self._outputCol}')
 
-        division_vector = self._get_division_vector(df)
+        # path to support mllib vectors
+        ml_mode = type(first_row[self._inputCol]) == DenseVector
+        division_vector = self._get_division_vector(df, ml_mode=ml_mode)
         divide_dense_vector_udf = F.udf(lambda v: DenseVector([0 if v[index] == 0 else \
                                             v[index] / division_vector[index] for index in range(len(v))]), VectorUDT())
         return df.withColumn(self._outputCol, divide_dense_vector_udf(self._inputCol))
 
-    def _get_division_vector(self, df):
+    def _get_division_vector(self, df, ml_mode=True):
+        # handle mllib vectors
+        if not ml_mode:
+            df = df.withColumn(self._inputCol, _convert_mllib_to_ml(self._inputCol))
         if self._norm == 'inf':
             # we should transform all vectors to positive values
             abs_df = df.withColumn(self._inputCol, _abs_dense_vector_udf(self._inputCol))
